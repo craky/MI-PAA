c0298536036f1af41b809c2ff3793dc774094c8d
craa <Vojtech Krakora>
2015-10-19 22:19:51 +0200

    Time measuring

--- knapsack/src/cz/cvut/fit/krakovoj/knapsackProblem.java
+++ knapsack/src/cz/cvut/fit/krakovoj/knapsackProblem.java
@@ -8,67 +8,87 @@ import java.io.InputStreamReader;
 import java.nio.charset.Charset;
 
 public class knapsackProblem {
-	
-	public static void main(String [] args){		
+
+	public static void main(String[] args) {
 		try {
 			knapsackHeuristic();
+			//knapsackProblemBruteForce();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
-	
-	public static void knapsackHeuristic() throws IOException{
+
+	public static void knapsackHeuristic() throws IOException {
 		String line;
-		InputStream fis = new FileInputStream("./data/knap_4.inst.dat");
-		InputStreamReader isr = new InputStreamReader(fis, Charset.forName("UTF-8"));
+		InputStream fis = new FileInputStream("./data/knap_40.inst.dat");
+		InputStreamReader isr = new InputStreamReader(fis,
+				Charset.forName("UTF-8"));
 		BufferedReader br = new BufferedReader(isr);
 		Knapsack knapsack = new Knapsack();
+		long startTime, estimatedTime = 0, totalTime = 0;
 		
-		while((line = br.readLine()) != null){
-			knapsack.fillKnapsack(line.split(" "));			
-			knapsack.bubbleSort();
-			knapsack.fillCapacity();
-			System.out.println("Best cost is " + knapsack.getSolutionCost() + " and weight is " + knapsack.getSolutionWeight());
+		while ((line = br.readLine()) != null) {
+			knapsack.fillKnapsack(line.split(" "));
+			for (int i = 0; i < 100000; i++) {
+				startTime = System.currentTimeMillis();
+				knapsack.bubbleSort();
+				knapsack.fillCapacity();
+				estimatedTime += System.currentTimeMillis() - startTime;
+			}
+			estimatedTime /=100000;
+			System.out.println(knapsack.getId() +"," + estimatedTime +"," + knapsack.getSolutionCost());
+			totalTime += estimatedTime;
+			estimatedTime = 0;
 			knapsack.clear();
 		}
-		
+		System.out.println("Total avarage time is " + (totalTime/50));
 		br.close();
 	}
 
-	
-	public static void knapsackProblemBruteForce() throws IOException{
+	public static void knapsackProblemBruteForce() throws IOException {
 		String line;
-		InputStream fis = new FileInputStream("./data/knap_4.inst.dat");
-		InputStreamReader isr = new InputStreamReader(fis, Charset.forName("UTF-8"));
+		InputStream fis = new FileInputStream("./data/knap_25.inst.dat");
+		InputStreamReader isr = new InputStreamReader(fis,
+				Charset.forName("UTF-8"));
 		BufferedReader br = new BufferedReader(isr);
 		Knapsack knapsack = new Knapsack();
 		KnapsackItem solution = new KnapsackItem();
-		
-		while((line = br.readLine()) != null){
+		long startTime, estimatedTime = 0, totalTime = 0;
+
+		while ((line = br.readLine()) != null) {
 			knapsack.fillKnapsack(line.split(" "));
-			knapsackProblemBruteForceRec(knapsack,0,solution);
-			System.out.println("Best cost is " + knapsack.getSolutionCost() + " and weight is " + knapsack.getSolutionWeight());
+			for (int i = 0; i <5; i++) {
+				startTime = System.currentTimeMillis();
+				knapsackProblemBruteForceRec(knapsack, 0, solution);
+				estimatedTime += System.currentTimeMillis() - startTime;
+			}
+			estimatedTime /=5;
+			System.out.println(knapsack.getId() +"," + estimatedTime +"," + knapsack.getSolutionCost());
+			totalTime += estimatedTime;
+			estimatedTime = 0;
 			knapsack.clear();
 			solution.clear();
 		}
-		
+		System.out.println("Total avarage time is " + (totalTime/50));
 		br.close();
 	}
-	
-	
-	public static void knapsackProblemBruteForceRec(Knapsack knapsack, int item, KnapsackItem solution){
-		KnapsackItem tempSolution = new KnapsackItem(solution.getCost(),solution.getWeight());
-		if (item >= knapsack.getSize()){
-			if (solution.getCost() > knapsack.getSolutionCost() && solution.getWeight() <= knapsack.getCapacity()){
+
+	public static void knapsackProblemBruteForceRec(Knapsack knapsack,
+			int item, KnapsackItem solution) {
+		KnapsackItem tempSolution = new KnapsackItem(solution.getCost(),
+				solution.getWeight());
+		if (item >= knapsack.getSize()) {
+			if (solution.getCost() > knapsack.getSolutionCost()
+					&& solution.getWeight() <= knapsack.getCapacity()) {
 				knapsack.setSolutionCost(solution.getCost());
 				knapsack.setSolutionWeight(solution.getWeight());
 			}
 			return;
 		}
-		knapsackProblemBruteForceRec(knapsack,item+1,tempSolution);
+		knapsackProblemBruteForceRec(knapsack, item + 1, tempSolution);
 		solution.increaseCost(knapsack.getItemCost(item));
 		solution.increaseWeight(knapsack.getItemWeight(item));
-		knapsackProblemBruteForceRec(knapsack,item+1,solution);
+		knapsackProblemBruteForceRec(knapsack, item + 1, solution);
 	}
 
 }
