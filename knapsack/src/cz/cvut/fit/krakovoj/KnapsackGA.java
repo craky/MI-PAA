a01792470067b6ad8c168b325fb092ff6be67b5e
craa <Vojtech Krakora>
2015-12-27 22:08:00 +0100

    Added class and test for GA. GA - Genetic Algorithm.

---
+++ knapsack/src/cz/cvut/fit/krakovoj/KnapsackGA.java
@@ -0,0 +1,13 @@
+package cz.cvut.fit.krakovoj;
+
+/**
+ * The Genetic algorithm for solution the knapsack problem.
+ * 
+ * @author krakovoj@fit.cvut.cz
+ * @version 1.0
+ * @since 2015-12-27
+ *
+ */
+public class KnapsackGA {
+
+}
