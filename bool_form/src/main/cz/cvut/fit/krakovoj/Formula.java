b86be2b1da1318771a9301f797cfe5fcfca29c0a
craa <Vojtech Krakora>
2016-01-17 10:11:30 +0100

    Formula reading and parsing data from file.

--- bool_form/src/main/cz/cvut/fit/krakovoj/Formula.java
+++ bool_form/src/main/cz/cvut/fit/krakovoj/Formula.java
@@ -3,6 +3,13 @@
  */
 package cz.cvut.fit.krakovoj;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -42,5 +49,42 @@ public class Formula {
 			cl.addLiteral(c.get(i));
 		}
 		clauses.add(cl.clone());
+	}
+	
+	public void readFromFile(String fileName) throws IOException{
+		String line;
+		InputStream fis = new FileInputStream(fileName);
+		InputStreamReader isr = new InputStreamReader(fis,
+				Charset.forName("UTF-8"));
+		BufferedReader br = new BufferedReader(isr);
+		
+		//First line are sums of literals & formulas
+		line = br.readLine();
+		//read line with weights
+		line = br.readLine();
+		parseWeights(line);
+		while ((line = br.readLine()) != null) {
+			parseClauses(line);
+		}
+		br.close();
+	}
+	
+	public void parseWeights(String line){
+		int weight, i = 0;
+		
+		while((weight = Integer.parseInt(line.split(" ")[i++])) != 0){
+			weights.add(weight);
+		}			
+	}
+	
+	public void parseClauses(String line){
+		int literal,i = 0;
+		Clause c = new Clause();
+		
+		while((literal = Integer.parseInt(line.split(" ")[i++])) != 0){
+			c.addLiteral(literal);
+		}
+		
+		clauses.add(c.clone());
 	}
 }
